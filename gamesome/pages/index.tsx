05ab970ec6b1e9414ab6bc7000c47c122a2c9eda
Dharaka Pranamya Mahadhika Damar <dharaka45@gmail.com>
2022-04-15 14:24:13 +0700

    create landing page

--- gamesome/pages/index.tsx
+++ gamesome/pages/index.tsx
@@ -1,84 +1,120 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
+// import Head from 'next/head'
 import Image from 'next/image'
+import bg from '../public/background.png'
+import hyperx from '../public/hyperx.png'
+import steelseries from '../public/steelseries.png'
+import razer from '../public/razer.png'
+import msi from '../public/msi.png'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div>
+      <div className="bg- flex h-[800px] w-full justify-center bg-[black]">
+        <div className="mt-72 h-full w-[1194px]">
+          <h1 className="text-8xl font-black text-white">Gamesome</h1>
+          <h2 className="mt-4 text-2xl text-white">
+            Upgrade your gaming experience!
+          </h2>
+          <div className="mt-12 h-[60px] w-[200px] justify-items-start rounded-full bg-[#1D5EDD]">
+            <div className="flex h-full items-center justify-center">
+              <h1 className="text-2xl text-base font-bold text-white">
+                Start Shopping
+              </h1>
+            </div>
+          </div>
         </div>
-      </main>
+      </div>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+      <div className="flex justify-center">
+        <div className="mt-[-90px] h-[180px] w-[1194px] rounded-2xl bg-[white] drop-shadow-xl">
+          <div className="mt-6 flex justify-center">
+            <h1 className="text-base font-bold">Popular Brand</h1>
+          </div>
+          <div className="mt-6 flex w-full justify-center">
+            <Image alt="hyperx" src={hyperx}></Image>
+            <Image alt="steelseries" src={steelseries}></Image>
+            <Image alt="razer" src={razer}></Image>
+            <Image alt="msi" src={msi}></Image>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center">
+        <div className="mt-48 flex w-[1194px] justify-center">
+          <div className="w-full">
+            <h1 className="text-5xl font-normal">Top Product</h1>
+
+            <div className="flex justify-start">
+              <div className="mr-4 mt-8 h-[408px] w-[220px] rounded-[10px] bg-[white] drop-shadow-[0_0_10px_rgba(0,0,0,0.25)]">
+                <div className="h-[200px] w-full rounded-[10px] bg-[black]"></div>
+                <div className="p-4">
+                  <h1 className="text-base font-normal">Product Name</h1>
+                  <h1 className="text-base font-bold">Rp 1.000.000</h1>
+                  <h1 className="mt-24">Terjual 999+</h1>
+                </div>
+              </div>
+
+              <div className="mr-4 mt-8 h-[408px] w-[220px] rounded-[10px] bg-[white] drop-shadow-[0_0_10px_rgba(0,0,0,0.25)]">
+                <div className="h-[200px] w-full rounded-[10px] bg-[black]"></div>
+                <div className="p-4">
+                  <h1 className="text-base font-normal">Product Name</h1>
+                  <h1 className="text-base font-bold">Rp 1.000.000</h1>
+                  <h1 className="mt-24">Terjual 999+</h1>
+                </div>
+              </div>
+
+              <div className="mr-4 mt-8 h-[408px] w-[220px] rounded-[10px] bg-[white] drop-shadow-[0_0_10px_rgba(0,0,0,0.25)]">
+                <div className="h-[200px] w-full rounded-[10px] bg-[black]"></div>
+                <div className="p-4">
+                  <h1 className="text-base font-normal">Product Name</h1>
+                  <h1 className="text-base font-bold">Rp 1.000.000</h1>
+                  <h1 className="mt-24">Terjual 999+</h1>
+                </div>
+              </div>
+
+              <div className="mr-4 mt-8 h-[408px] w-[220px] rounded-[10px] bg-[white] drop-shadow-[0_0_10px_rgba(0,0,0,0.25)]">
+                <div className="h-[200px] w-full rounded-[10px] bg-[black]"></div>
+                <div className="p-4">
+                  <h1 className="text-base font-normal">Product Name</h1>
+                  <h1 className="text-base font-bold">Rp 1.000.000</h1>
+                  <h1 className="mt-24">Terjual 999+</h1>
+                </div>
+              </div>
+
+              <div className="mr-4 mt-8 h-[408px] w-[220px] rounded-[10px] bg-[white] drop-shadow-[0_0_10px_rgba(0,0,0,0.25)]">
+                <div className="h-[200px] w-full rounded-[10px] bg-[black]"></div>
+                <div className="p-4">
+                  <h1 className="text-base font-normal">Product Name</h1>
+                  <h1 className="text-base font-bold">Rp 1.000.000</h1>
+                  <h1 className="mt-24">Terjual 999+</h1>
+                </div>
+              </div>
+            </div>
+
+            <h1 className="mt-48 text-5xl font-normal">Featured Product</h1>
+
+            <h1 className="mt-48 flex justify-center text-5xl font-normal">
+              Need an upgrade for your
+            </h1>
+            <h1 className="mt-4 flex justify-center text-5xl font-normal">
+              gaming experience?
+            </h1>
+
+            <div className="flex justify-center">
+              <div className="mt-12 h-[60px] w-[200px] rounded-full bg-[#1D5EDD]">
+                <div className="flex h-full items-center justify-center">
+                  <h1 className="text-2xl text-base font-bold text-white">
+                    Start Shopping
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-24">adsasdasd</div>
     </div>
   )
 }
